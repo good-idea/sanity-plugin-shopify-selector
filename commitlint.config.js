734b9a76540db4702924ce5b58216d5cc1562ebb
Joseph Thomas <joseph@good-idea.studio>
2018-10-23 14:43:27 -0700

    chore(repo): converts to monorepo

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+// [
+// 	'build',
+// 	'ci',
+// 	'chore',
+// 	'docs',
+// 	'feat',
+// 	'fix',
+// 	'perf',
+// 	'refactor',
+// 	'revert',
+// 	'style',
+// 	'test'
+// ]
+
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'scope-empty': [2, 'never'],
+		'scope-enum': [2, 'always', ['repo', 'plugin', 'proxy']],
+	},
+}
