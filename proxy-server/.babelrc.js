b033b9342abf3758d62415f7374aa9d2fcda3cb1
Joseph Thomas <joseph@good-idea.studio>
2018-11-25 18:20:46 -0800

    feat(proxy): set up config for publishing

--- proxy-server/.babelrc.js
+++ proxy-server/.babelrc.js
@@ -1,1 +1,20 @@
-module.exports = {}
+module.exports = {
+	presets: [
+		'@babel/preset-flow',
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: 'current',
+				},
+			},
+		],
+	],
+	plugins: [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-syntax-export-default-from',
+		'ramda',
+	],
+}
